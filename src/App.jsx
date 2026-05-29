c56a15436083d903db29a45a835f380f4e515b91
starlight-cloud-env <jarjarbc@gmail.com>
2026-05-28 23:52:52 -0400

    New foundation for Aurora

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './routes'
 
 function App() {
-  
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  )
 }
 
 export default App
--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,16 @@
-import React from React;
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Browse from './pages/Browse'
+import Watch from './pages/Watch'
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/browse" element={<Browse />} />
+      <Route path="/watch/:id" element={<Watch />} />
+    </Routes>
+  )
+}
+
+export default AppRoutes
