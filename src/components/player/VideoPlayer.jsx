8b1f6886ac115f7236cf3e0f18b544ade18b43d9
starlight-cloud-env <jarjarbc@gmail.com>
2026-05-29 22:52:06 -0400

    potential fix for mobile viewer

--- src/components/player/VideoPlayer.jsx
+++ src/components/player/VideoPlayer.jsx
@@ -1,6 +1,8 @@
 import './VideoPlayer.css'
 
 function VideoPlayer({ embedUrl }) {
+  if (!embedUrl) return null
+
   return (
     <div className="player">
       <div className="player__wrapper">
@@ -8,7 +10,8 @@ function VideoPlayer({ embedUrl }) {
           src={embedUrl}
           className="player__iframe"
           allowFullScreen
-          allow="autoplay; fullscreen; picture-in-picture"
+          allow="autoplay; fullscreen; picture-in-picture; encrypted-media"
+          scrolling="no"
           title="Aurora Video Player"
         />
       </div>
